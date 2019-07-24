b55695dc9bbc794858ce6c3074856d163f51b01a
chenchao <wenbushi@gmail.com>
2019-07-24 09:48:11 +0800

    waitgroup

---
+++ wg/snippet.go
@@ -0,0 +1,77 @@
+// -*- mode:go;mode:go-playground -*-
+// snippet of code @ 2019-07-21 23:38:50
+
+// === Go Playground ===
+// Execute the snippet with Ctl-Return
+// Remove the snippet completely with its dir and all files M-x `go-playground-rm`
+
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+var tokens = make(chan struct{}, 3)
+
+type foo struct {
+	ch  chan int
+	res chan int
+	wg  sync.WaitGroup
+}
+
+var now = time.Now()
+
+func (f *foo) Put(s int) {
+	fmt.Println("Put:\t", time.Since(now).Round(time.Second), s)
+	f.ch <- s
+}
+
+func (f *foo) Run() {
+	for i := range f.ch {
+		f.wg.Add(1)
+		fmt.Println("WGADD:\t", f.wg)
+		go func(i int) {
+			tokens <- struct{}{}
+			fmt.Println("Run:\t", time.Since(now).Round(time.Second), i)
+			defer func() {
+				f.wg.Done()
+				fmt.Println("WGDONE:\t", f.wg)
+			}()
+			f.res <- i * i
+			time.Sleep(5 * time.Second)
+			<-tokens
+		}(i)
+	}
+}
+
+func (f *foo) Close() {
+	close(f.ch)
+	f.wg.Wait()
+	close(f.res)
+}
+
+func (f *foo) Results() <-chan int {
+	return f.res
+}
+
+func main() {
+	ch := make(chan int)
+	res := make(chan int)
+	f := foo{ch: ch, res: res}
+
+	go f.Run()
+
+	for i := 0; i < 10; i++ {
+		f.Put(i)
+	}
+
+	go f.Close()
+
+	for r := range f.Results() {
+		fmt.Println("Result:\t", time.Since(now).Round(time.Second), r)
+	}
+
+	fmt.Println("finished")
+}
